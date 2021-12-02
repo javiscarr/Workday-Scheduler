a0d4c6f178725f9aad691f4cb5013a74c6283279
Doonywoony <javismcarr@gmail.com>
2021-12-02 13:02:51 -0500

    rows added for timeblocks

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+//jquery button click (function)
+
+//local storage for set item
+// create values to pass through 
+//value for time variable 
+//hour key value description
+
+
+// new function to update hour
+//create var using moment js (to get current number of hours)
+//loop over timeblock .each is looping format
+//condition statements to get current time and timeblock are equal to define as past,present of future
+//set interval for 15000
+//load any saved data from local storage by calling out the key
+//display the current day on the page by using .text using moment js
+});
+
